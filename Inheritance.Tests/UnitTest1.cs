7930fac2ff94041c519dee48e838cd5b6b786a4c
E1ijahJ <elijahguijoseph@gmail.com>
2025-02-27 23:51:16 -0500

    Added testing for the quarter class

--- Inheritance.Tests/UnitTest1.cs
+++ Inheritance.Tests/UnitTest1.cs
@@ -27,4 +27,21 @@ public class UnitTest1
         }
 
     }
+    [Fact]
+    public void Quarter_BasicFunctionality()
+    {
+        
+        Assert.Equal(new Quarter(0.1), new Quarter(0.2)); 
+        Assert.Equal(new Quarter(0.3), new Quarter(0.4)); 
+        Assert.Equal(new Quarter(0.6), new Quarter(0.7)); 
+        Assert.Equal(new Quarter(0.8), new Quarter(0.9)); 
+
+        
+        Assert.True(new Quarter(0.1) < new Quarter(0.3)); 
+        Assert.True(new Quarter(0.5) < new Quarter(0.75));
+
+    
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Quarter(-0.1)); 
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Quarter(1.1)); 
+    }
 }
